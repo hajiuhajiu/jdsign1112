6ab6fcea09bd3fe9d11f7a96426d333a8cb1ca0e
Thomas Lin <bill56789@126.com>
2021-05-03 02:15:49 +0800

    Update jd_jxfactory.js

--- jd_jxfactory.js
+++ jd_jxfactory.js
@@ -1332,7 +1332,7 @@ function requireConfig() {
     console.log(`开始获取${$.name}配置文件\n`);
     //Node.js用户请在jdCookie.js处填写京东ck;
     const shareCodes = $.isNode() ? require('./jdDreamFactoryShareCodes.js') : '';
-    console.log("JD互助码"+shareCodes)
+    console.log("JD互助码"+shareCodes.length)
     console.log(`共${cookiesArr.length}个京东账号\n`);
     $.shareCodesArr = [];
     if ($.isNode()) {
