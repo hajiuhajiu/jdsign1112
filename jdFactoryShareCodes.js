758ed38ecd7d53bab58ba2b3280833713e8012a5
Thomas Lin <bill56789@126.com>
2021-05-02 20:21:02 +0800

    Update jdFactoryShareCodes.js

--- jdFactoryShareCodes.js
+++ jdFactoryShareCodes.js
@@ -8,25 +8,6 @@
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let shareCodes =[]
-// 判断github action里面是否有京喜工厂互助码 
-if (process.env.DREAM_FACTORY_SHARE_CODES) {
-  if (process.env.DREAM_FACTORY_SHARE_CODES.indexOf('&') > -1) {
-    console.log(`您的互助码选择的是用&隔开\n`)
-    shareCodes = process.env.DREAM_FACTORY_SHARE_CODES.split('&');
-  } else if (process.env.DREAM_FACTORY_SHARE_CODES.indexOf('\n') > -1) {
-    console.log(`您的互助码选择的是用换行隔开\n`)
-    shareCodes = process.env.DREAM_FACTORY_SHARE_CODES.split('\n');
-  } else {
-    shareCodes = process.env.DREAM_FACTORY_SHARE_CODES.split();
-  }
-} else if (process.env.DREAM_FACTORY_SHARE_CODES) {
-  console.log(`由于您secret里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
-}
-for (let i = 0; i < shareCodes.length; i++) {
-  const index = (i + 1 === 1) ? '' : (i + 1);
-  exports['shareCodes' + index] = shareCodes[i];
-}
-
 // 判断github action里面是否有东东工厂互助码
 if (process.env.DDFACTORY_SHARECODES) {
   if (process.env.DDFACTORY_SHARECODES.indexOf('&') > -1) {
